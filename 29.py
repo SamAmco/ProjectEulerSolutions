17525523fdbd7c1a03f18fcc333fd9871aff7a94
Sam Amco <sam@mantle.tech>
2016-10-22 21:51:09 +0100

    solution to 29.

--- 29.py
+++ 29.py
@@ -1,3 +1,16 @@
-for num in range(2,9+1):
-	for power in range(2, 9+1):
-		print(str(pow(num, power)) + "=" + str(num) + "^" + str(power))
+import math
+
+cache = []
+dupCount = 0
+gridSize = 100 + 1
+for a in range(2, gridSize):
+	for b in range(2, gridSize):
+		if ((a, b) in cache):
+			continue
+		for c in range(a+1, gridSize):
+			for d in range(2, gridSize):
+				if (math.isclose(d / math.log(a, c), b)):
+					dupCount += 1
+					cache.append((c, d))
+
+print(((gridSize - 2) * (gridSize - 2)) - dupCount)
