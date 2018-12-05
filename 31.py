ecb9655433764368eaac440bf2dd3fb8fe5e9bd5
Sam Amco <samuel.amco@btinternet.com>
2018-12-05 02:22:09 +0000

    made more compact and concise.

--- 31.py
+++ 31.py
@@ -1,41 +1,13 @@
 coin_values = [200,100,50,20,10,5,2,1]
 
-class Solution:
-    def __init__(self, coins):
-        self.coins = sorted(coins)
-
-    def __repr__(self):
-        return ','.join(str(x) for x in self.coins)
-
-    def __eq__(self, other):
-        return self.coins == other.coins
-
-    def __hash__(self):
-        return hash(self.__repr__())
-
-
-class SolutionCounter:
-    def __init__(self):
-        self.solutions = set()
-
-    def add_solution(self, solution):
-        self.solutions.add(Solution(solution))
-
-
-def count_ways_to_make_X_recursive(current_count, previous_coins, solution_counter, X):
-    #print("Received: \n%s \n%s \n%s \n%s" % (current_count, previous_coins, solution_counter, X))
+def count_ways_to_make_X(current_count, X, last_coin = coin_values[0]):
     if current_count > X:
-        return
+        return 0
     elif current_count == X:
-        solution_counter.add_solution(previous_coins)
+        return 1
 
-    allowed_coins = coin_values
-    if(current_count > 0):
-        allowed_coins = coin_values[coin_values.index(previous_coins[-1]):]
+    allowed_coins = coin_values[coin_values.index(last_coin):]
 
-    for v in allowed_coins:
-        count_ways_to_make_X_recursive(current_count + v, list(previous_coins) + [v], solution_counter, X)
+    return sum(map(lambda v : count_ways_to_make_X(current_count + v, X, v), allowed_coins))
 
-solution_counter = SolutionCounter()
-count_ways_to_make_X_recursive(0, [], solution_counter, 200)
-print(len(solution_counter.solutions))
+print(count_ways_to_make_X(0, 200))
